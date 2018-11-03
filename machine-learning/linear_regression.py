60291d2edd45eb2ddc4a184f531fcb22ba5866db
Bo <bopeng.data@gmail.com>
2018-11-03 12:31:00 -0700

    Update linear_regression.py

--- machine-learning/linear_regression.py
+++ machine-learning/linear_regression.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-% matplotlib inline
 
 
 class LinearRegression(object):
@@ -10,25 +8,24 @@ class LinearRegression(object):
         pass
 
     def train(self, x, y, epochs, learning_rate):
-        self.theta_array = np.zeros(x.shape[1])
+        self.theta_array = np.zeros(x.shape[0])
 
         for _ in range(0, epochs):
             for index, theta in enumerate(self.theta_array):
-                average_full_batch_loss = 0.5*np.average((np.dot(x[index], np.array(theta).transpose) - y)**2)
-                average_full_batch_partial_derrivitives = np.average(np.dot(x[index], np.array(theta).transpose)- y)
-                self.theta_array[index] = theta - learning_rate * average_full_batch_partial_derrivitives
+                avg_full_batch_loss = 0.5*np.average(np.square(np.dot(x[index], theta) - y))
+                avg_full_batch_partial_derrivitives = np.average(np.dot(x[index], theta)- y)
+                self.theta_array[index] = theta - learning_rate * avg_full_batch_partial_derrivitives
 
     def validate(self, x, y):
-#         assert 'self.theta_array' in globals(), ("ValueError: theta is not defined. Please make sure to train the model "
-#             "before validating.")
+        # assert 'self.theta_array' in globals(), ("ValueError: theta is not defined."
+        #     "Please make sure to train the model before validating.")
 
-        predicted_y = np.dot(x, self.theta_array.transpose)
-        accuracy_score = np.sqrt(np.average((y- predicted_y)**2)) # Root Mean Square Error (RMSE)
-        return predicted_y, accuracy_score
+        predicted_y = np.dot(x, self.theta_array)
+        rmse = np.sqrt(np.average(np.square(y- predicted_y))) # Root Mean Square Error (RMSE)
+        return predicted_y, rmse
 
     def predict(self, x):
-#         assert 'self.theta_array' in globals(), ("ValueError: theta is not defined. Please make sure to train the model "
-#             "before predicting.")
-
-        predicted_y = np.dot(x, self.theta_array.transpose)
+        # assert 'self.theta_array' in globals(), ("ValueError: theta is not defined."
+        #     "Please make sure to train the model before predicting.")
+        predicted_y = np.dot(self.theta_array, x)
         return predicted_y
