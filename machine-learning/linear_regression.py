b15ab50cadfcbb4b2b60c80cc457252a5f949031
Bo <bopeng.data@gmail.com>
2018-11-03 11:15:05 -0700

    Update linear_regression.py

--- machine-learning/linear_regression.py
+++ machine-learning/linear_regression.py
@@ -1,25 +1,34 @@
 import numpy as np
+import matplotlib.pyplot as plt
+% matplotlib inline
 
 
 class LinearRegression(object):
-    """ Initially this is a linear regression with theta1 and theta0 as parameters and using
-        batch gradient descent.
-    """
+    """ Linear regression model using batch gradient descent"""
 
     def __init__(self):
-        self.theta1 = 0
+        pass
 
     def train(self, x, y, epochs, learning_rate):
+        self.theta_array = np.zeros(x.shape[1])
+
         for _ in range(0, epochs):
-            average_full_batch_loss = 0.5*np.average((np.dot(x, self.theta1) - y)**2)
-            average_full_batch_partial_derrivitives = np.average(np.dot(x, self.theta1)- y)
-            self.theta1 = self.theta1 - learning_rate * average_full_batch_partial_derrivitives
+            for index, theta in enumerate(self.theta_array):
+                average_full_batch_loss = 0.5*np.average((np.dot(x[index], np.array(theta).transpose) - y)**2)
+                average_full_batch_partial_derrivitives = np.average(np.dot(x[index], np.array(theta).transpose)- y)
+                self.theta_array[index] = theta - learning_rate * average_full_batch_partial_derrivitives
 
     def validate(self, x, y):
-        predicted_y = np.dot(x, self.theta1)
+#         assert 'self.theta_array' in globals(), ("ValueError: theta is not defined. Please make sure to train the model "
+#             "before validating.")
+
+        predicted_y = np.dot(x, self.theta_array.transpose)
         accuracy_score = np.sqrt(np.average((y- predicted_y)**2)) # Root Mean Square Error (RMSE)
         return predicted_y, accuracy_score
 
     def predict(self, x):
-        predicted_y = np.dot(x, self.theta1)
+#         assert 'self.theta_array' in globals(), ("ValueError: theta is not defined. Please make sure to train the model "
+#             "before predicting.")
+
+        predicted_y = np.dot(x, self.theta_array.transpose)
         return predicted_y
