3d87d0873fbdc0332478eb97b611d195bccc07e0
Bo <bopeng.data@gmail.com>
2018-11-03 22:35:44 -0700

    Update linear_regression

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Machine-Learning-from-Scratch
 Coding through machine learning from scratch using tutorials and other code as references.
 
-Some algorithms I may explore are linear regression, logistic regression, feed forward neural networks, Convolutional Neural Networks, 
-Recurrent Neural Networks using LSTM/GRUs, GANs and deep reinforcement learning.
-
-Some materials used as references to build the code are [Andrew Ng's machine learning course on coursera](https://www.coursera.org/learn/machine-learning) and [Erik Linder-Norén's ML-From-Scratch repo.](https://github.com/eriklindernoren/ML-From-Scratch)
+Some algorithms I may explore are linear regression, logistic regression, feed forward neural networks,
+Convolutional Neural Networks, Recurrent Neural Networks using LSTM/GRUs, GANs and deep reinforcement learning.
--- machine-learning/linear_regression.py
+++ machine-learning/linear_regression.py
@@ -2,30 +2,49 @@ import numpy as np
 
 
 class LinearRegression(object):
-    """Multvariate Linear regression model using batch gradient descent"""
+    """Multivariate linear regression model using batch gradient descent"""
 
     def __init__(self):
         pass
 
     def train(self, x, y, epochs, learning_rate):
-        self.theta_array = np.zeros(x.shape[0])
 
-        for _ in range(0, epochs):
+        self.theta_array = np.zeros(np.array(x.ndim)+1)
+
+        x = self._add_bias(x)
+
+        for _ in range(1, epochs):
             for index, theta in enumerate(self.theta_array):
-                avg_full_batch_loss = 0.5*np.average(np.square(np.dot(x[index], theta) - y))
-                avg_full_batch_partial_derrivitives = np.average(np.dot(x[index], theta)- y)
-                self.theta_array[index] = theta - learning_rate * avg_full_batch_partial_derrivitives
+                avg_full_batch_loss = np.average(np.sqrt(np.square(np.dot(x[index], theta) - y)))
+                avg_full_batch_partial_derrivitives = 0.5*np.average((np.dot(x[index], theta)- y)*x[index])
+                self.theta_array[index] -= learning_rate * avg_full_batch_partial_derrivitives
 
     def validate(self, x, y):
-        # assert 'self.theta_array' in globals(), ("ValueError: theta is not defined."
-        #     "Please make sure to train the model before validating.")
+        self._check_theta_exists()
 
-        predicted_y = np.dot(x, self.theta_array)
+        x = self._add_bias(x)
+
+        predicted_y = np.dot(x.transpose(), self.theta_array)
         rmse = np.sqrt(np.average(np.square(y- predicted_y))) # Root Mean Square Error (RMSE)
         return predicted_y, rmse
 
     def predict(self, x):
-        # assert 'self.theta_array' in globals(), ("ValueError: theta is not defined."
-        #     "Please make sure to train the model before predicting.")
-        predicted_y = np.dot(self.theta_array, x)
+        self._check_theta_exists()
+
+        x = self._add_bias(x)
+
+        predicted_y = np.dot(x.transpose(), self.theta_array)
         return predicted_y
+
+    def _add_bias(self, x):
+        if x.ndim == 1:
+             x = np.row_stack((x, np.ones(len(x))))
+        else:
+             x = np.row_stack((x, np.ones(len(x[0]))))
+        return x
+
+    def _check_theta_exists(self):
+        assert hasattr(self, 'theta_array'), ("ValueError: theta is not defined. "
+            "Please make sure to train the model before predicting.")
+
+    
