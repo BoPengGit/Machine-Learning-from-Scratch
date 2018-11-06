91a08541067465eea0090ef033d0988688e96415
Bo <bopeng.data@gmail.com>
2018-11-05 19:43:55 -0800

    Update Linear Regression

--- machine-learning/linear_regression.py
+++ machine-learning/linear_regression.py
@@ -7,21 +7,24 @@ class LinearRegression(object):
     def __init__(self):
         pass
 
-    def train(self, x, y, epochs, learning_rate):
+    def train(self, x, y, batch_size=32, epochs=10, learning_rate=0.001):
 
         self.theta_array = np.zeros(np.array(x.ndim)+1)
 
         x = self._add_bias(x)
 
         for _ in range(1, epochs):
+            i = 0
             for index, theta in enumerate(self.theta_array):
                 avg_batch_loss = np.average(
                                  np.sqrt(
                                  np.square(
-                                 np.dot(x[index], theta) - y)))
-                avg_batch_partial_derrivitive = 0.5*np.average((
-                                                    np.dot(x[index], theta)- y)*x[index])
-                self.theta_array[index] -= learning_rate * avg_batch_partial_derrivitive
+                                 np.dot(x[index][i:i+32], theta[i:i+32]) - y[i:i+32])))
+                avg_batch_partial_deriv = 0.5*np.average((
+                                            np.dot(x[index][i:i+32], theta[i:i+32])- y) *x[index][i:i+32])
+                self.theta_array[index] -= learning_rate * avg_batch_partial_deriv
+
+                i += batch_size
 
     def validate(self, x, y):
         self._check_theta_exists()
--- machine-learning/logistic_regression.py
+++ machine-learning/logistic_regression.py
@@ -4,29 +4,53 @@ import numpy as np
 class LinearRegression(object):
     """Multvariate Logistic regression model using batch gradient descent"""
 
+
     def __init__(self):
         pass
 
-    def train(self, x, y, epochs, learning_rate):
-        self.theta_array = np.zeros(x.shape[0])
+    def train(self, x, y, batch_size=32, epochs=10, learning_rate=0.001):
 
-        for _ in range(0, epochs):
+        self.theta_array = np.zeros(np.array(x.ndim)+1)
+
+        x = self._add_bias(x)
+
+        for _ in range(1, epochs):
+            i = 0
             for index, theta in enumerate(self.theta_array):
-                sigmoid_loss = - np.log(h(x))
-                avg_full_batch_loss = 0.5*np.average(np.square(np.dot(x[index], theta) - y))
-                avg_full_batch_partial_derrivitives = np.average(np.dot(x[index], theta)- y)
-                self.theta_array[index] = theta - learning_rate * avg_full_batch_partial_derrivitives
+                avg_batch_loss = np.average(
+                                 np.sqrt(
+                                 np.square(
+                                 np.dot(x[index][i:i+32], theta[i:i+32]) - y[i:i+32])))
+                avg_batch_partial_deriv = 0.5*np.average((
+                                            np.dot(x[index][i:i+32], theta[i:i+32])- y) *x[index][i:i+32])
+                self.theta_array[index] -= learning_rate * avg_batch_partial_deriv
+
+                i += batch_size
 
     def validate(self, x, y):
-        # assert 'self.theta_array' in globals(), ("ValueError: theta is not defined."
-        #     "Please make sure to train the model before validating.")
+        self._check_theta_exists()
 
-        predicted_y = np.dot(x, self.theta_array)
-        accuracy = len(y) - np.sum(np.absolute(y_predicted - y))
-        return predicted_y, accuracy
+        x = self._add_bias(x)
+
+        predicted_y = np.dot(x.transpose(), self.theta_array)
+        rmse = np.sqrt(np.average(np.square(y- predicted_y))) # Root Mean Square Error (RMSE)
+        return predicted_y, rmse
 
     def predict(self, x):
-        # assert 'self.theta_array' in globals(), ("ValueError: theta is not defined."
-        #     "Please make sure to train the model before predicting.")
-        predicted_y = np.dot(self.theta_array, x)
+        self._check_theta_exists()
+
+        x = self._add_bias(x)
+
+        predicted_y = np.dot(x.transpose(), self.theta_array)
         return predicted_y
+
+    def _add_bias(self, x):
+        if x.ndim == 1:
+             x = np.row_stack((x, np.ones(len(x))))
+        else:
+             x = np.row_stack((x, np.ones(len(x[0]))))
+        return x
+
+    def _check_theta_exists(self):
+        assert hasattr(self, 'theta_array'), ("ValueError: theta is not defined. "
+            "Please make sure to train the model before predicting.")
