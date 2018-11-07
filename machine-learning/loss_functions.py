e43dc8f2d1ed869c714ab405edfe3788b02db075
Bo <bopeng.data@gmail.com>
2018-11-06 23:16:32 -0800

    Create loss_functions.py file

---
+++ machine-learning/loss_functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def cross_entropy(x, theta_array):
+    sigmoid = 1/(1+np.exp(-np.dot(x.transpose(), theta_array)))
+    return sigmoid
+
+
+def rmse(x, theta_array, y):
+    # root mean square error
+    avg_minibatch_loss = np.sqrt(
+             np.average(
+             np.square(
+             x.transpose().dot(theta_array) - y)))
+    return avg_minibatch_loss
+
+def svm_loss(x, theta_array, y):
+    pass
